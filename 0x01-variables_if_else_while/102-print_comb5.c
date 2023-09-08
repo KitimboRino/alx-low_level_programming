e69be91e80076180f6c49fa4edb5b778c9206163
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-09-08 08:16:06 -0700

    12. Software is eating the World task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,24 +5,34 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int i, j;
+int i, j, k, l;
 
-for (i = 0; i <= 99; i++)
+/* Loop through two numbers from 00 to 99*/
+for (i = 0; i < 100; i++)
 {
-for (j = i; j <= 99; j++)
+for (j = i + 1; j < 100; j++)
 {
-putchar((i / 10) + '0'); /* Print the tens digit of the first number*/
-putchar((i % 10) + '0'); /* Print the ones digit of the first number*/
+/* Extract tens and ones digits of the first number (i)*/
+k = i / 10;
+l = i % 10;
 
+/* Print the tens and ones digits of the first number with a space*/
+putchar(k + '0'); /* Print the tens digit*/
+putchar(l + '0'); /* Print the ones digit*/
 putchar(' '); /* Print a space*/
 
-putchar((j / 10) + '0'); /* Print the tens digit of the second number*/
-putchar((j % 10) + '0'); /* Print the ones digit of the second number*/
+/* Extract tens and ones digits of the second number (j)*/
+k = j / 10;
+l = j % 10;
 
-if (i < 99 || j < 99)
+/* Print the tens and ones digits of the second number*/
+putchar(k + '0'); /* Print the tens digit*/
+putchar(l + '0'); /* Print the ones digit*/
+
+/* Add a comma and space for all combinations except the last one*/
+if (i != 98 || j != 99)
 {
 putchar(','); /* Print a comma*/
 putchar(' '); /* Print a space*/
@@ -34,3 +44,4 @@ putchar('\n'); /* Print a new line*/
 
 return (0);
 }
+
