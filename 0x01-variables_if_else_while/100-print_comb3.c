e4e3da4654bc744885c8b8e51136cc4b27059cda
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-09-08 07:50:18 -0700

    10. Inventing is a combination of brains and materials. The more brains you use, the less material you need task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,30 +5,27 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int i, j, k;
+int i, j;
 
-/* Nested loops to create combinations of two digits*/
-for (i = 0; i < 8; i++)
+for (i = 0; i <= 8; i++)
 {
-for (j = i + 1; j < 10; j++)
+for (j = i + 1; j <= 9; j++)
 {
-k = 10 * i + j; /* Create a two-digit number*/
+putchar(i + '0'); /* Print the tens digit*/
+putchar(j + '0'); /* Print the ones digit*/
 
-putchar((k / 10) + '0'); /* Print the tens digit*/
-putchar((k % 10) + '0'); /* Print the ones digit*/
-
-/* Add a comma and space for all combinations except the last one*/
 if (i < 8 || j < 9)
 {
-putchar(',');
-putchar(' ');
+putchar(','); /* Print a comma*/
+putchar(' '); /* Print a space*/
 }
 }
 }
 
-putchar('\n'); /*Print a new line*/
+putchar('\n'); /* Print a new line*/
 
 return (0);
 }
