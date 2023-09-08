f8abe63461d73ef1c8875f446a510fd87be132d6
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-09-08 06:57:37 -0700

    5. Numbers task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+/* Loop through numbers from 0 to 9*/
+for (i = 0; i < 10; i++)
+{
+printf("%d", i); /* Print the current number*/
+}
+
+printf("\n"); /* Print a new line*/
+
+return (0);
+}
