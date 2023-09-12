ba9852d07fc234499bac86c3eec5bbf0b908cd56
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-09-12 12:57:06 -0700

    refactor: added comments 0-putchar.c task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,18 @@
  *
  * Return: void
  */
+
 void print_alphabet(void)
 {
+/* Declare a variable to stroe the current character*/
 char letter;
 
+/* Loop through the lowercase alphabet, starting from a and ending at z*/
 for (letter = 'a'; letter <= 'z'; letter++)
 {
+/* Call the _putchar function to print the current letter to the output*/
 _putchar(letter);
 }
+/* Print a neline character to the ouptu, creating a new line*/
 _putchar('\n');
 }
