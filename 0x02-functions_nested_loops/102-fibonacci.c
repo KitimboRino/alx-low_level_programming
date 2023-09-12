7cabd95e05f80eb507eba68d1cb32ffe34cb9406
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-09-12 09:05:29 -0700

    feat: 102-fibonacci.c task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 int n = 50; /* Number of Fibonacci numbers to print*/
-long int fib1 = 1, fib2 = 2, next;
+int i, long int fib1 = 1, fib2 = 2, next;
 
 /* Print the first two Fibonacci numbers*/
 printf("%ld, %ld", fib1, fib2);
 
 /* Calculate and print the remaining Fibonacci numbers*/
-for (int i = 3; i <= n; i++)
+for (i = 3; i <= n; i++)
 {
 next = fib1 + fib2;
 printf(", %ld", next);
