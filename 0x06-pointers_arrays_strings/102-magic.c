24a6758d237f4d7964b034aa7e2cca4ee746df50
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-09-21 05:45:58 -0700

    feat: 10. A dream doesn't become reality through magic; it takes sweat, determination and hard work task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,6 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
+  ;
   *p -= 926;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
