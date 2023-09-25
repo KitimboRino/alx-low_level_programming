7c748317b2f98d1cc46a3bda8139a31f74249905
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-09-25 04:53:43 -0700

    feat: 8. Double pointer, double fun

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -6,6 +6,7 @@
  * @s: Pointer to a char pointer (double pointer)
  * @to: Pointer to a char (string)
  */
-void set_string(char **s, char *to) {
+void set_string(char **s, char *to)
+{
 *s = to; /* Set the value of the pointer s to the address of to */
 }
