0e04c4085bedb4906889620ed58e5b9bb50277ed
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-09-25 03:34:37 -0700

    feat: 2. strchr task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,12 +14,12 @@ char *_strchr(char *s, char c)
 {
 while (*s != '\0')
 {
-if(*s == c)
+if (*s == c)
 {
-return s; // Return a pointer to the first occurrence of c
+return (s); /* Return a pointer to the first occurrence of c */
 }
-s++; // Move to the next character in the string
+s++; /* Move to the next character in the string */
 }
-// If character c is not found, return NULL
+/* If character c is not found, return NULL */
 return (NULL);
 }
