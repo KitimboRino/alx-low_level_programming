ee6c365c98774276eb7e13b969f0186ddc9470c5
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-09-28 04:21:47 -0700

    feat: 4. Once an idea has taken hold of the brain it's almost impossible to eradicate task

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,10 +13,9 @@ int _pow_recursion(int x, int y)
 if (y < 0)
 /* Error: Negative exponent is not defined */
 return (-1);
-else if (y == 0)
+if (y == 0)
 /* Base case: Any number raised to the power of 0 is 1 */
 return (1);
-else
 /* Recursive calculation of x^y */
 return (x * _pow_recusion(x, y - 1));
 }
