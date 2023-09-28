effd183b7cb85f1b047709e9a40bca0bbcd7ffe8
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-09-28 05:03:53 -0700

    feat: 6. Inception. Is it possible? task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,30 +9,16 @@
  */
 
 int is_prime_number(int n)
+if (n == 2)
 {
-if (n <= 1)
-{
-return (0); /* 0 and 1 are not prime numbers */
+return (1);
 }
-else if (n <= 3)
+else if (n > 1 && n % 2 == 1)
 {
-return (1); /* 2 and 3 are prime numbers */
-}
-else if (n % 2 == 0 || n % 3 == 0)
-{
-return (0); /* Multiples of 2 or 3 are not prime numbers */
+return (1);
 }
 else
 {
-int i;
-for (i = 5; i * i <= n; i += 6)
-{
-if (n % i == 0 || n % (i + 2) == 0)
-{
-/* If n is divisible by any number in the form of 6k ± 1, it's not prime*/
 return (0);
-}
-}
-return (1); /* n is a prime number */
 }
 }
