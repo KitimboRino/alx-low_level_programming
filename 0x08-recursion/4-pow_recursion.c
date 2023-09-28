90b8b4f19244e476d4659d488649a64f78ab4140
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-09-28 04:16:45 -0700

    feat: 4. Once an idea has taken hold of the brain it's almost impossible to eradicate task

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+/**
+ * _pow_recursion - Calculates x raised to the power of y
+ * recursively,.
+ * @x: The base number.
+ * @y: The exponent.
+ *
+ * Return: The result of x^y, or -1 if y is negative.
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+/* Error: Negative exponent is not defined */
+return (-1);
+else if (y == 0)
+/* Base case: Any number raised to the power of 0 is 1 */
+return (1);
+else
+/* Recursive calculation of x^y */
+return (x * _pow_recusion(x, y - 1));
+}
