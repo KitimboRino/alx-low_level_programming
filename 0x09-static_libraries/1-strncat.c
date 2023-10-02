b5e3293083f50797883b5e67c18cd4c2ef204e22
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-10-02 05:53:03 -0700

    feat: 0. A library is not a luxury but one of the necessities of life task

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,55 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strncat - Concatenates two strings.
- * @dest: Pointer to the destination string.
- * @src: Pointer to the source string.
- * @n: Maximum number of bytes to be used from src.
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
- * Return: Pointer to the resulting string dest.
+ * Return: dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-int dest_len = 0;
-int i = 0;
+int i;
+int j;
 
-while (dest[dest_len] != '\0')
-dest_len++;
-
-while (i < n && src[i] != '\0')
+i = 0;
+while (dest[i] != '\0')
 {
-dest[dest_len] = src[i];
 i++;
-dest_len++;
 }
-
-dest[dest_len] = '\0';
-
-return (dest);
-}
-
-/**
- * main - Check the _strncat function.
- *
- * Return: Always 0.
- */
-
-int main(void)
+j = 0;
+while (j < n && src[j] != '\0')
 {
-char s1[98] = "Hello ";
-char s2[] = "World!\n";
-char *ptr;
-
-printf("%s\n", s1);
-printf("%s", s2);
-ptr = _strncat(s1, s2, 1);
-printf("%s\n", s1);
-printf("%s", s2);
-printf("%s\n", ptr);
-ptr = _strncat(s1, s2, 1024);
-printf("%s", s1);
-printf("%s", s2);
-printf("%s", ptr);
-return (0);
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
 }
