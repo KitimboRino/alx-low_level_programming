316902dae18207ba762562fbcf87f8154bbf671b
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-10-19 11:15:45 -0700

    fix: 2. Add node task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,8 +6,9 @@ void first(void) __attribute__ ((constructor));
  * first - This function is executed before the main
  *	function and prints a sentence
  */
+
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,28 +3,39 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list
- * @str: new string to add in the node
+ * add_node - adds a new node at the beginning of a linked list.
+ * @head: A double pointer to the head of the linked list.
+ * @str: The new string to be added in the node.
  *
  * Return: the address of the new element, or NULL if it fails
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	unsigned int len = 0;
+/* Declare a pointer to a new list node. */
+list_t *new;
 
-	while (str[len])
-		len++;
+/* Initialize a variable to store the length of the input string. */
+unsigned int len = 0;
 
-	new = malloc(sizeof(list_t));
-	if (!new)
-		return (NULL);
+/* Calculate the length of the input string. */
+while (str[len])
+len++;
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
+/* Allocate memory for the new node. */
+new = malloc(sizeof(list_t));
+if (!new)
+return (NULL);
 
-	return (*head);
+/* Copy the inout string to the new node. */
+new->str = strdup(str);
+
+/* Set the lenght and the next pointer for the new node. */
+new->len = len;
+new->next = (*head);
+
+/* Update the head to point to the new node. */
+(*head) = new;
+
+return (*head);
 }
