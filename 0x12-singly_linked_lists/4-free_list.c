8175ea84e3db3698beb50bb2f4686e6ac061db4d
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-10-19 04:11:41 -0700

    feat: 4. Free list task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list.
+ * @head: Pointer to the head of the list_t list to be freed.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	/* Loop through the list and free each node and its string. */
+	while (head)
+	{
+		/* Store the next node to avoid losing the reference. */
+		temp = head->next;
+		/* Free the string stored in the node. */
+		free(head->str);
+		/* Free the current node. */
+		free(head);
+		/* Move to the next node. */
+		head = temp;
+	}
+}
