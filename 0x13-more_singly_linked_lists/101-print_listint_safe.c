93857c63367bbe7b81df05ab3acae310dd6f2bf3
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-10-24 08:10:23 -0700

    fix: 12. Print (safe version) task and run tests

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,100 @@
-#include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely
- * using the Floyd's Cycle Detection algorithm.
- * @head: A pointer to the head of the list.
+ * looped_listint_len - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ *
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
+ */
+
+size_t looped_listint_len(const listint_t *head)
+{
+const listint_t *tortoise, *hare;
+size_t nodes = 1;
+if (head == NULL || head->next == NULL)
+return (0);
+
+tortoise = head->next;
+hare = (head->next)->next;
+
+while (hare)
+{
+if (tortoise == hare)
+{
+/*Loop detected, calculate the number of unique nodes in the loop*/
+tortoise = head;
+while (tortoise != hare)
+{
+nodes++;
+tortoise = tortoise->next;
+hare = hare->next;
+}
+
+/* Continue traversing to find where the loop begins */
+tortoise = tortoise->next;
+while (tortoise != hare)
+{
+nodes++;
+tortoise = tortoise->next;
+}
+
+return (nodes);
+}
+
+tortoise = tortoise->next;
+hare = (hare->next)->next;
+}
+
+/* If no loop is detetced, return 0 */
+return (0);
+}
+
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ *
+ * @head: A pointer to the head of the listint_t list.
  *
  * Return: The number of nodes in the list.
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-size_t count = 0;
-const listint_t *slow = head;
-const listint_t *fast = head;
+size_t nodes, index = 0;
 
-while (fast != NULL && fast->next != NULL)
-{
-printf("[%p] %d\n", (void *)slow, slow->n);
-count++;
-slow = slow->next;
-fast = fast->next->next;
+nodes = looped_listint_len(head);
 
-if (slow == fast)
+if (nodes == 0)
 {
-printf("[%p] %d\n", (void *)slow, slow->n);
-printf("-> [%p] %d\n", (void *)fast->next, fast->next->n);
-count++;
-break;
+/* If the list is not looped, print each node and count them */
+for (; head != NULL; nodes++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
 }
 }
 
-return (count);
+else
+{
+/* Loop, print unique nodes and indicate where the loop begins*/
+for (index = 0; index < nodes; index++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+
+printf("-> [%p] %d\n", (void *)head, head->n);
+}
+
+/* Return the total number of nodes */
+return (nodes);
 }
