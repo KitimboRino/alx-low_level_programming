fdbfc83ad5e5f23c8792654ce57fa59ccafe0350
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-10-26 02:25:56 -0700

    feat: 2. 10 task and run tests

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+/**
+ * get_bit - Gets the value of a bit at a given index.
+ * @n: The unsigned long integer.
+ * @index: The index of the bit to retrieve (0 being the least significant bit).
+ *
+ * Return: The value of the bit at the given index or -1 if an error occurs.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index >= sizeof(unsigned long int) * 8)
+{
+/* Return -1 for an out-of-range index. */
+return (-1);
+}
+
+/* Shift the number to the right and check the bit at the given index. */
+return (n >> index) & 1;
+}
