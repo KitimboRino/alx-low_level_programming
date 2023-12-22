5945cc7a13e3832da8d6eb947e229856a4cd1e05
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-12-21 23:03:05 -0800

    feat: 6. >>> del ht tasks and run tests

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - free hash table and all nodes
+ * @ht: pointer to hash table
+ *
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+    hash_node_t *bucket, *aux_free;
+    unsigned long int i = 0;
+
+/* Check if the hash table is NULL */
+if (!ht)
+return;
+
+/* Iterate through each index of the hash table array */
+for (i = 0; i < ht->size; i++)
+{
+bucket = ht->array[i];
+
+/* Iterate through the linked list at each index */
+while (bucket)
+{
+aux_free = bucket;
+bucket = bucket->next;
+
+/* Free the key, value, and the node itself */
+if (aux_free->key)
+free(aux_free->key);
+if (aux_free->value)
+free(aux_free->value);
+free(aux_free);
+}
+}
+
+/* Free the array and the hash table structure */
+free(ht->array);
+free(ht);
+}
